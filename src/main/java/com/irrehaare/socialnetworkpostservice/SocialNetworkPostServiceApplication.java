4149522e8d13f09bbda0356049bc7aa5ecd94742
irrehaare <wjtkwilk@gmail.com>
2022-08-04 21:03:26 +0200

    Initial commit

---
+++ src/main/java/com/irrehaare/socialnetworkpostservice/SocialNetworkPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.irrehaare.socialnetworkpostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkPostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irrehaare/socialnetworkpostservice/SocialNetworkPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irrehaare.socialnetworkpostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
